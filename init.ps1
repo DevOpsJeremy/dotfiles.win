b8cc6ab4b1e82e6fbedf926187572ff55fe1e29a
DevOps Jeremy <142703512+DevOpsJeremy@users.noreply.github.com>
2026-03-05 23:49:11 -0800

    Update script

--- init.ps1
+++ init.ps1
@@ -1,7 +1,11 @@
 param (
-    $ScriptPath = "https://raw.githubusercontent.com/DevOpsJeremy/dotfiles.win/refs/heads/init/init.ps1",
-    $ChocoInstall = "https://community.chocolatey.org/install.ps1"
+    $Owner = "DevOpsJeremy",
+    $Repo = "dotfiles.win"
+    $Branch = "init",
+    $ScriptPath = "init.ps1",
+    $ChocoInstallUri = "https://community.chocolatey.org/install.ps1"
 )
+$scriptUri = "https://raw.githubusercontent.com/{0}/{1}/refs/heads/{2}/{3}" -f $Owner, $Repo, $Branch, $ScriptPath
 
 # Check if admin
 $winId = [System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -11,13 +15,13 @@ $isAdmin = $winPrincipal.IsInRole($adminRole)
 
 $ps = (Get-Command powershell).Source
 if (-not $isAdmin) {
-    Start-Process $ps -ArgumentList "-Command", "Invoke-RestMethod '$ScriptPath' | Invoke-Expression"
+    Start-Process $ps -ArgumentList "-Command", "Invoke-RestMethod '$scriptUri' | Invoke-Expression"
 }
 
 # Install Chocolatey if not already
 try {
     Get-Command choco -ErrorAction Stop | Out-Null
 } catch {
-    Invoke-RestMethod $ChocoInstall | Invoke-Expression
+    Invoke-RestMethod $ChocoInstallUri | Invoke-Expression
 }
 
