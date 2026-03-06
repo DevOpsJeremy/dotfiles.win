4814de86823a6d230ecc01256a28ff02b4405f0b
DevOps Jeremy <142703512+DevOpsJeremy@users.noreply.github.com>
2026-03-06 00:21:01 -0800

    re-add choco package

--- init.ps1
+++ init.ps1
@@ -1,17 +1,33 @@
 param (
     $Owner = "DevOpsJeremy",
-    $Repo = "dotfiles.win"
+    $Repo = "dotfiles.win",
     $Branch = "init",
     $ScriptPath = "init.ps1",
+    $PackagesPath = "packages.config",
     $ChocoInstallUri = "https://community.chocolatey.org/install.ps1"
 )
-$scriptUri = "https://raw.githubusercontent.com/{0}/{1}/refs/heads/{2}/{3}" -f $Owner, $Repo, $Branch, $ScriptPath
+#region Functions
+function checkAdmin {
+    $winId = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $winPrincipal = [System.Security.Principal.WindowsPrincipal]::new($winId)
+    $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+    return $winPrincipal.IsInRole($adminRole)
+}
+function ghUrlRaw {
+    param (
+        $p,
+        $o = $script:Owner,
+        $r = $script:Repo,
+        $b = $script:Branch
+    )
+    return "https://raw.githubusercontent.com/{0}/{1}/refs/heads/{2}/{3}" -f $o, $r, $b, $p
+}
+#endregion Functions
+
+$scriptUri = ghUrlRaw $ScriptPath
 
 # Check if admin
-$winId = [System.Security.Principal.WindowsIdentity]::GetCurrent()
-$winPrincipal = [System.Security.Principal.WindowsPrincipal]::new($winId)
-$adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
-$isAdmin = $winPrincipal.IsInRole($adminRole)
+$isAdmin = checkAdmin
 
 $ps = (Get-Command powershell).Source
 if (-not $isAdmin) {
@@ -25,3 +41,8 @@ try {
     Invoke-RestMethod $ChocoInstallUri | Invoke-Expression
 }
 
+$packagesUri = ghUrlRaw $PackagesPath
+$packagesFileName = [IO.Path]::GetFileName($PackagesPath)
+$packagesFilePath = Join-Path ([IO.Path]::GetTempPath()) $packagesFileName
+Invoke-RestMethod $packagesUri -OutFile $packagesFilePath
+choco install $packagesFilePath
