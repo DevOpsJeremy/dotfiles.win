edd9cda7771f8d991b9f423ce1a3cb290e1da8f3
DevOps Jeremy <142703512+DevOpsJeremy@users.noreply.github.com>
2026-03-05 23:00:34 -0800

    Add echo

--- init.ps1
+++ init.ps1
@@ -20,3 +20,5 @@ try {
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
+
+echo Done
