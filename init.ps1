a727452a75d60b8ee3e02371af82874772871bd6
DevOps Jeremy <142703512+DevOpsJeremy@users.noreply.github.com>
2026-03-05 23:10:32 -0800

    Install choco

--- init.ps1
+++ init.ps1
@@ -1,13 +1,15 @@
 param (
-    $ScriptPath = "https://raw.githubusercontent.com/DevOpsJeremy/dotfiles.win/refs/heads/init/init.ps1"
+    $ScriptPath = "https://raw.githubusercontent.com/DevOpsJeremy/dotfiles.win/refs/heads/init/init.ps1",
+    $ChocoInstall = "https://community.chocolatey.org/install.ps1"
 )
 
 # Check if admin
 $winId = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $winPrincipal = [System.Security.Principal.WindowsPrincipal]::new($winId)
 $adminRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+$isAdmin = $winPrincipal.IsInRole($adminRole)
 
-if (-not $winPrincipal.IsInRole($adminRole)) {
+if (-not ) {
     $ps = (Get-Command powershell).Source
     Start-Process $ps -ArgumentList "-NoExit", "-Command", "Invoke-RestMethod '$ScriptPath' | Invoke-Expression" -Verb RunAs
 }
@@ -16,9 +18,13 @@ if (-not $winPrincipal.IsInRole($adminRole)) {
 try {
     Get-Command choco -ErrorAction Stop | Out-Null
 } catch {
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    if ($isAdmin) {
+        Invoke-RestMethod $ChocoInstall | Invoke-Expression
+    } else {
+        $ps = (Get-Command powershell).Source
+        Start-Process $ps -ArgumentList "-Command", "Invoke-RestMethod '$ChocoInstall' | Invoke-Expression" -Verb RunAs -Wait
+        Start-Process $ps -ArgumentList "-Command", "Invoke-RestMethod '$ScriptPath' | Invoke-Expression"
+        exit
+    }
 }
 
-echo Done
