f00ae2da681f036489bc3175266d11826224ea72
DevOps Jeremy <142703512+DevOpsJeremy@users.noreply.github.com>
2026-03-09 01:05:58 -0700

    Add logic for existing local repo

--- init.ps1
+++ init.ps1
@@ -17,23 +17,20 @@ function ghUrlZip {
 }
 function getRepo {
     param (
-        $outDir = $PWD.Path,
-        $destName = $(Join-Path $script:LocalRepoRoot $Repo),
+        $TempDirName = 'repo-temp',
+        $destName = $(Join-Path $script:LocalRepoRoot $script:Repo),
         $r = $script:Repo,
         $b = $script:Branch
     )
     Begin {
-        $destDir = if (Test-Path $outDir -PathType Container) {
-            Get-Item (Resolve-Path $outDir).Path
-        } elseif (Test-Path $outDir -PathType Leaf) {
-            throw "The path '$outDir' exists and is not a directory"
-        } else {
-            New-Item $outDir -ItemType Directory -Force
-        }
+        # Get the temp directory to save our files
+        $tmp = [IO.Path]::GetTempPath()
+        $destDir = Join-Path $tmp $TempDirName
+        New-Item $destDir -ItemType Directory -Force | Out-Null
         $repoDestName = "{0}-{1}" -f $r, $b
         $repoDestPath = Join-Path $destDir $repoDestName
         $url = ghUrlZip
-        $tmpFile = ([IO.Path]::GetTempFileName()) + ".zip"
+        $tmpFile = Join-Path $destDir "$TempDirName.zip"
     }
     Process {
         Invoke-RestMethod $url -OutFile $tmpFile
@@ -41,10 +38,36 @@ function getRepo {
         return Move-Item $repoDestPath $destName -PassThru -Force
     }
     End {
-        Remove-Item $tmpFile -ErrorAction SilentlyContinue -Force
+        Remove-Item $destDir -ErrorAction SilentlyContinue -Recurse -Force
     }
 }
 #endregion Functions
+
+$localRepoPath = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath((Join-Path $LocalRepoRoot $Repo))
+
+# If repo path exists and is a file, fail
+if (Test-Path $localRepoPath -PathType Leaf) {
+    throw "The path '$localRepoPath' exists and is not a directory"
+    exit 1
+}
+
+# If repo directory exists, prompt user before deleting and recreating
+if (Test-Path $localRepoPath) {
+    $no = 1
+    $answer = $Host.UI.PromptForChoice(
+        "Rebuild local repo?", # Caption
+        "The '$localRepoPath' directory exists. Recreate?", # Message
+        @('&Yes', '&No'), # Choices
+        $no # Default: No
+    )
+
+    if ($answer -eq $no) {
+        Write-Host "Exiting."
+        exit
+    }
+
+    Remove-Item $localRepoPath -Force -Recurse
+}
 
 # Download the repo
 $repoPath = getRepo
