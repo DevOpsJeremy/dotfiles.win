c20a75bc1d3604a91102ff0f4f74ff51797dd3f8
DevOps Jeremy <142703512+DevOpsJeremy@users.noreply.github.com>
2026-03-17 00:14:39 -0700

    Fix params

--- setup/init.build.ps1
+++ setup/init.build.ps1
@@ -1,12 +1,19 @@
+param (
+    $ChocoPackagesPath = (Resolve-Path "setup/packages.config").Path,
+    $ChocoInstallUri = "https://community.chocolatey.org/install.ps1",
+    $ChocoOutputDir = $PSScriptRoot,
+    $ChocoPackagesDir = (Resolve-Path "setup/packages").Path,
+    $WinUtilsPath = (Resolve-Path "setup/winutils.config.json").Path,
+    $WinUtilsUri = "https://christitus.com/win"
+)
+
 task install-chocolatey {
     # Install Chocolatey if not already
     try {
         Get-Command choco -ErrorAction Stop | Out-Null
     } catch {
-        Invoke-RestMethod $Task.Data.ChocoInstallUri | Invoke-Expression
+        Invoke-RestMethod $ChocoInstallUri | Invoke-Expression
     }
-} -Data @{
-    ChocoInstallUri = "https://community.chocolatey.org/install.ps1"
 }
 
 task winutils {
@@ -15,29 +22,22 @@ task winutils {
 
     # Configure Windows tweaks
     # This script takes control of the console, so launch in a new window
-    Start-Process $ps -ArgumentList "-Command", "& ([ScriptBlock]::Create((Invoke-RestMethod $Task.Data.WinUtilsUri))) -Config '$((Resolve-Path $Task.Data.WinUtilsPath).Path)' -Run -Noui" -Wait
-} -Data @{
-    WinUtilsUri     = "https://christitus.com/win"
-    WinUtilsPath    = Resolve-Path "setup/winutils.config.json"
+    Start-Process $ps -ArgumentList "-Command", "& ([ScriptBlock]::Create((Invoke-RestMethod $WinUtilsUri))) -Config '$((Resolve-Path $WinUtilsPath).Path)' -Run -Noui" -Wait
 }
 
 task choco-pack {
-    Get-ChildItem -Path $Task.Data.PackagesDir -Filter "*.nuspec" -File -Recurse |
+    Get-ChildItem -Path $ChocoPackagesDir -Filter "*.nuspec" -File -Recurse |
         ForEach-Object {
-            choco pack "$($_.FullName)" --outputdirectory "$Task.Data.OutputDirectory"
+            choco pack "$($_.FullName)" --outputdirectory "$ChocoOutputDir"
         }
-} -Data @{
-    PackagesDir     = Resolve-Path "setup/packages"
-    OutputDirectory = $PSScriptRoot
 }
 
 task choco-install {
     # Install Chocolatey packages
-    choco install -y $Task.Data.PackagesPath
-} -Data @{
-    PackagesPath = Resolve-Path "setup/packages.config"
+    choco install -y $ChocoPackagesPath
 }
 
 task setup {
     echo "hello world"
+    $ChocoPackagesPath
 }
